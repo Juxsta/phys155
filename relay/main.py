2a201c8f49bad7d493f8c3002e9a841b39d12705
Eric Reyes <ericreyes@Erics-MacBook-Pro.local>
2021-12-07 12:00:31 -0800

    fixed relay code

--- relay/main.py
+++ relay/main.py
@@ -9,12 +9,7 @@ gate = DigitalInOut(board.D1) #activation voltage of npn transistor
 button.direction = Direction.INPUT
 gate.direction = Direction.OUTPUT
 
-open_loop = True
 while True:
-    if open_loop and button.value:
-        open_loop = True #relay is open and load is off
-        gate.value = False
-    elif !open_loop and button.value:
-        open_loop = False #relay is closed and load is on
-        gate.value = True
-    time.sleep(2)
+    if button.value:
+        gate.value = ~gate.value
+    time.sleep(.1)
